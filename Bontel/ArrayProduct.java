a7715ef3771f6596ebd6aa5ef08cfd706632c379
Adelanwa Hammed Bolaji <120419965+Licitguy@users.noreply.github.com>
2023-01-19 17:45:02 +0100

    Add files via upload

---
+++ Bontel/ArrayProduct.java
@@ -0,0 +1,18 @@
+package Bontel;
+
+import java.util.Arrays;
+
+public class ArrayProduct {
+    public static void main(String[] args) {
+        int [] numbers = {30,41,20,43,18};
+        int sum = 0;
+       for(int number : numbers){
+           sum += number;
+       }
+        System.out.println(sum);
+
+
+
+
+    }
+}
